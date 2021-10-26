3afdf9a90198bf60ef9b8e9acc5bfc46a5982bcd
codehiker1997 <faizantanveer84@gmail.com>
2021-10-26 00:24:24 -0700

    This is my second commit

--- connect.py
+++ connect.py
@@ -1,1 +1,18 @@
+import requests
+
 API = "0f6a756cb3deccf53598b1e855c5c191"
+
+movie_id = 500
+
+
+api_version = 4
+api_base_url = "https://api.themoviedb.org/{api_version}"
+end_point =f'/movie/{movie_id}'
+
+endpoint = f"{api_base_url}{end_point}?api_key={API}"
+# print(endpoint)
+r = requests.get(endpoint)
+
+
+print(r.status_code)
+print(r.text)
